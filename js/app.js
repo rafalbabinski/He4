5b37031828a80c5679111005b28c386b88a571a2
rafalbabinski <rafalbabinski@github.com>
2018-06-27 08:26:03 +0200

    delete removechild doubled offer

--- js/app.js
+++ js/app.js
@@ -11,20 +11,6 @@ const offers = [{
     status12m: '+0,42%',
     status30m: '+0,85%',
     statusytd: '+1,31%'
-},
-{
-    category: 'Category1',
-    title: 'Ethasd Classic',
-    company: 'Ethereum foundation',
-    boostDescription: 'Altcoins - highest growth',
-    totalPrice: 1800,
-    currency: 'EUR',     
-    status1d: '+1,31%',
-    status1m: '+1,78%',
-    status3m: '+2,25%',
-    status12m: '+3,22%',
-    status30m: '+4,43%',
-    statusytd: '+5,11%'
 }]
 
 function setOffers() {
@@ -48,10 +34,6 @@ function setOffers() {
 
         document.querySelector('body').appendChild(offerCopy)
     })
-
-    // delete first - offer with null elements
-    const firstOffer = document.querySelector('.offer');
-    firstOffer.parentElement.removeChild(firstOffer);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
